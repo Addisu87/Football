7642f90492580ec7e41bc516faf3f3ad98d12415
Addisu <addisuhaile87@gmail.com>
2023-03-14 21:33:30 +0300

    Update navigation to navigate between screens

--- App.js
+++ App.js
@@ -5,8 +5,16 @@ import { TailwindProvider } from "tailwindcss-react-native";
 import HomeScreen from "./screens/HomeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DiscoverScreen from "./screens/DiscoverScreen";
-import { Icon } from "react-native-vector-icons/FontAwesome";
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { BlurView } from "expo-blur";
+import StandingsScreen from "./screens/StandingsScreen";
+import MoreScreen from "./screens/MoreScreen";
+import {
+  ArrowsPointingOutIcon,
+  ChartBarIcon,
+  HomeIcon,
+  SquaresPlusIcon,
+} from "react-native-heroicons/outline";
 
 // const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -15,7 +23,19 @@ export default function App() {
   return (
     <TailwindProvider>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarStyle: { position: "absolute" },
+            tabBarActiveTintColor: "#1808f1",
+            tabBarBackground: () => (
+              <BlurView
+                tint="light"
+                intensity={100}
+                style={StyleSheet.absoluteFill}
+              />
+            ),
+          }}
+        >
           <Tab.Screen
             name="Home"
             component={HomeScreen}
@@ -25,10 +45,53 @@ export default function App() {
                   Home
                 </Text>
               ),
-              tabBarIcon: () => <Icon name="rocket" size={30} color="#900" />,
+              tabBarIcon: ({ color, size }) => (
+                <HomeIcon color={color} size={size} />
+              ),
             }}
           />
-          <Tab.Screen name="Discover" component={DiscoverScreen} />
+          <Tab.Screen
+            name="Discover"
+            component={DiscoverScreen}
+            options={{
+              tabBarLabel: () => (
+                <Text className="focus:text-white focus:outline-none text-gray-500">
+                  Discover
+                </Text>
+              ),
+              tabBarIcon: ({ color, size }) => (
+                <ArrowsPointingOutIcon color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Standings"
+            component={StandingsScreen}
+            options={{
+              tabBarLabel: () => (
+                <Text className="focus:text-white focus:outline-none text-gray-500">
+                  Standings
+                </Text>
+              ),
+              tabBarIcon: ({ color, size }) => (
+                <ChartBarIcon color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="More"
+            component={MoreScreen}
+            options={{
+              tabBarLabel: () => (
+                <Text className="focus:text-white focus:outline-none text-gray-500">
+                  More
+                </Text>
+              ),
+              tabBarIcon: ({ color, size }) => (
+                <SquaresPlusIcon color={color} size={size} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </TailwindProvider>
