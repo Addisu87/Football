1752b7a57ac0938d098642ee4832c62cfff2bff7
Addisu <addisuhaile87@gmail.com>
2023-03-14 18:59:38 +0300

    Update navigation method

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { TailwindProvider } from "tailwindcss-react-native";
 import HomeScreen from "./screens/HomeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DiscoverScreen from "./screens/DiscoverScreen";
+import { Icon } from "react-native-vector-icons/FontAwesome";
+import { Text } from "react-native";
 
 // const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -14,7 +16,18 @@ export default function App() {
     <TailwindProvider>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: () => (
+                <Text className="focus:text-white focus:outline-none text-gray-500">
+                  Home
+                </Text>
+              ),
+              tabBarIcon: () => <Icon name="rocket" size={30} color="#900" />,
+            }}
+          />
           <Tab.Screen name="Discover" component={DiscoverScreen} />
         </Tab.Navigator>
       </NavigationContainer>
