fc5be90d30410c24ea4b49ed1e3f38ccb9699496
Addisu <addisuhaile87@gmail.com>
2023-03-14 21:39:25 +0300

    Remove headings and add SafeAreaView for different screens

--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const MoreScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>MoreScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
