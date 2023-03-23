16c54bf4fc8779f8e2a9e7505fe6b6688a6ebdd4
Addisu <addisuhaile87@gmail.com>
2023-03-23 09:17:44 +0300

    Create news item

---
+++ components/NewsItem.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const NewsItem = () => {
+  return (
+    <View>
+      <Text>NewsItem</Text>
+    </View>
+  );
+};
+
+export default NewsItem;
