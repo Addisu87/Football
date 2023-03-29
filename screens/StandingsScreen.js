fba840ee0b96d585ab67756ca76722f41580bebd
Addisu <addisuhaile87@gmail.com>
2023-03-29 20:15:20 +0300

    Update table contents

--- screens/StandingsScreen.js
+++ screens/StandingsScreen.js
@@ -1,9 +1,20 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { Row, Table } from "react-native-table-component";
+
+const tableData = {
+  tableHead: ["Rank", "Team", "MP", "W", "D", "L", "P"],
+  widthArr: [50, 90, 45, 45, 45, 45, 45],
+  tableData: [
+    ["1", "Arsenal", "21", "15", "4", "1", "65"],
+    ["2", "Man United", "21", "14", "1", "6", "59"],
+    ["3", "Chelsea", "21", "10", "5", "6", "45"],
+  ],
+};
 
 const StandingsScreen = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState(tableData);
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
@@ -14,38 +25,36 @@ const StandingsScreen = () => {
 
   return (
     <SafeAreaView>
-      <View>
-        <Text>England: Premier League</Text>
-      </View>
+      <View className="flex-1 p-4 pt-8 bg-white">
+        <ScrollView horizontal={true}>
+          <View>
+            <View>
+              <Text className="font-bold text-lg">England: Premier League</Text>
+            </View>
 
-      <table class="table-auto">
-        <thead>
-          <tr>
-            <th>
-              <Text>Rank</Text>
-            </th>
-            <th>
-              <Text>Team</Text>
-            </th>
-            <th>
-              <Text>MP</Text>
-            </th>
-            <th>
-              <Text>W</Text>
-            </th>
-            <th>
-              <Text>D</Text>
-            </th>
-            <th>
-              <Text>L</Text>
-            </th>
-            <th>
-              <Text>P</Text>
-            </th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
+            <Table>
+              <Row
+                data={data.tableHead}
+                widthArr={data.widthArr}
+                className="h-11 bg-slate-700 font-semibold text-base  items-center justify-center text-cyan-900"
+              />
+            </Table>
+
+            <ScrollView>
+              <Table>
+                {data.tableData.map((rowData, index) => (
+                  <Row
+                    key={index}
+                    data={rowData}
+                    widthArr={data.widthArr}
+                    className="h-14 bg=[#E7E6E1] text-base font-semibold m-2 text-center"
+                  />
+                ))}
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
