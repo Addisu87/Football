73965520b588469bbc8f0ec3b4e330e7fd573c26
Addisu <addisuhaile87@gmail.com>
2023-03-30 18:19:13 +0300

    Create matchSlice

---
+++ features/matchSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+export const matchSlice = createSlice({
+  name: "match",
+  initialState,
+  reducers: {
+    addMatch: (state, action) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeMatch: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addMatch, removeMatch } = matchSlice.actions;
+
+export default matchSlice.reducer;
--- features/store.js
+++ features/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import matchReducer from "./matchSlice";
 import standingReducer from "./standingSlice";
 import teamReducer from "./teamSlice";
 
 export default configureStore({
   reducer: {
     team: teamReducer,
+    match: matchReducer,
     standing: standingReducer,
   },
 });
