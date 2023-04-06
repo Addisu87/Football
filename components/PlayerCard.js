ad28f9e18c4e362c99b307cb7cfabcff835dcf31
Addisu <addisuhaile87@gmail.com>
2023-04-06 17:23:22 +0300

    Styling using grid playercard component

--- components/PlayerCard.js
+++ components/PlayerCard.js
@@ -3,14 +3,19 @@ import { View, Text, Image } from "react-native";
 
 const PlayerCard = ({ Photo, Name, Age, Position }) => {
   return (
-    <View className="text-center items-center justify-center flex-row space-x-2 mx-">
-      <Image source={Photo} className="w-20 h-20 rounded-full drop-shadow-lg" />
-      <View className="space-y-1">
-        <Text className="text-neutral-500 dark:text-neutral-400">{Name}</Text>
-        <Text className="text-neutral-500 dark:text-neutral-400">{Age}</Text>
-        <Text className="text-neutral-500 dark:text-neutral-400">
-          {Position}
-        </Text>
+    <View className="grid grid-cols-3 gap-4">
+      <View className="text-center items-center justify-center flex-row space-x-2 mx-">
+        <Image
+          source={Photo}
+          className="w-20 h-20 rounded-full drop-shadow-lg"
+        />
+        <View className="space-y-1">
+          <Text className="text-neutral-500 dark:text-neutral-400">{Name}</Text>
+          <Text className="text-neutral-500 dark:text-neutral-400">{Age}</Text>
+          <Text className="text-neutral-500 dark:text-neutral-400">
+            {Position}
+          </Text>
+        </View>
       </View>
     </View>
   );
