88fb415666647945e139e88699dee8de9ca465b4
Addisu <addisuhaile87@gmail.com>
2023-04-07 18:48:32 +0300

    Update useRoute for playerCard

--- components/PlayerCard.js
+++ components/PlayerCard.js
@@ -1,25 +1,63 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
-const PlayerCard = ({ Photo, Name, Age, Position }) => {
+const PlayerCard = ({
+  Photo,
+  Name,
+  Age,
+  Nationality,
+  TeamLogo,
+  TeamName,
+  Position,
+  Appearance,
+  Passes,
+  Goals,
+  Cards,
+}) => {
+  const navigation = useNavigation();
+
   return (
-    <View className="grid grid-cols-2 gap-4">
-      <View className="items-center justify-center flex-row space-x-2">
-        <View>
-          <Image
-            source={Photo}
-            className="w-20 h-20 rounded-full drop-shadow-lg"
-          />
-        </View>
-        <View className="space-y-1">
-          <Text className="text-neutral-700 dark:text-neutral-800">{Name}</Text>
-          <Text className="text-neutral-700 dark:text-neutral-800">{Age}</Text>
-          <Text className="text-neutral-700 dark:text-neutral-800">
-            {Position}
-          </Text>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("playerStatistics", {
+          Photo,
+          Name,
+          Age,
+          Nationality,
+          TeamLogo,
+          TeamName,
+          Position,
+          Appearance,
+          Passes,
+          Goals,
+          Cards,
+        })
+      }
+      className="bg-white mr-3 shadow"
+    >
+      <View className="grid grid-cols-2 gap-4">
+        <View className="items-center justify-center flex-row space-x-2">
+          <View>
+            <Image
+              source={Photo}
+              className="w-20 h-20 rounded-full drop-shadow-lg"
+            />
+          </View>
+          <View className="space-y-1">
+            <Text className="text-neutral-700 dark:text-neutral-800">
+              {Name}
+            </Text>
+            <Text className="text-neutral-700 dark:text-neutral-800">
+              {Age}
+            </Text>
+            <Text className="text-neutral-700 dark:text-neutral-800">
+              {Position}
+            </Text>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
--- components/Players.js
+++ components/Players.js
@@ -1,31 +1,12 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  TouchableOpacity,
-  ActivityIndicator,
-  SafeAreaView,
-} from "react-native";
+import { View, ActivityIndicator, SafeAreaView } from "react-native";
 import PlayerCard from "./PlayerCard";
 import { getPlayersData } from "../api/API";
 import NotFound from "./NotFound";
-import { useNavigation } from "@react-navigation/native";
 
-const Players = ({
-  Photo,
-  Name,
-  Age,
-  Nationality,
-  TeamLogo,
-  TeamName,
-  Position,
-  Appearance,
-  Passes,
-  Goals,
-  Cards,
-}) => {
+const Players = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [playerData, setPlayerData] = useState([]);
-  const navigation = useNavigation();
 
   useEffect(() => {
     setIsLoading(true);
@@ -39,56 +20,38 @@ const Players = ({
 
   return (
     <SafeAreaView>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate("playerStatistics", {
-            Photo,
-            Name,
-            Age,
-            Nationality,
-            TeamLogo,
-            TeamName,
-            Position,
-            Appearance,
-            Passes,
-            Goals,
-            Cards,
-          })
-        }
-      >
-        {isLoading ? (
-          <View className=" flex-1 items-center justify-center">
-            <ActivityIndicator size="large" color="#0B646B" />
-          </View>
-        ) : (
-          <>
-            {playerData.length > 0 ? (
-              <View>
-                {playerData?.map((play, index) => (
-                  <PlayerCard
-                    key={index}
-                    Photo={{ uri: play?.player?.photo }}
-                    Name={play?.player?.name}
-                    Age={play?.player?.age}
-                    Nationality={play?.player?.nationality}
-                    TeamLogo={play?.statistics[0]?.team?.logo}
-                    TeamName={play?.statistics[0]?.team?.name}
-                    Position={play?.statistics[0]?.games?.position}
-                    Appearance={play?.statistics[0]?.games?.appearances}
-                    Goals={play?.statistics[0]?.goals?.total}
-                    Passes={play?.statistics[0]?.passes?.total}
-                    Cards={play?.statistics[0]?.cards?.yellowred}
-                  />
-                ))}
-              </View>
-            ) : (
-              <>
-                <NotFound />
-              </>
-            )}
-          </>
-        )}
-      </TouchableOpacity>
+      {isLoading ? (
+        <View className=" flex-1 items-center justify-center">
+          <ActivityIndicator size="large" color="#0B646B" />
+        </View>
+      ) : (
+        <>
+          {playerData.length > 0 ? (
+            <View>
+              {playerData?.map((play, index) => (
+                <PlayerCard
+                  key={index}
+                  Photo={{ uri: play?.player?.photo }}
+                  Name={play?.player?.name}
+                  Age={play?.player?.age}
+                  Nationality={play?.player?.nationality}
+                  TeamLogo={play?.statistics[0]?.team?.logo}
+                  TeamName={play?.statistics[0]?.team?.name}
+                  Position={play?.statistics[0]?.games?.position}
+                  Appearance={play?.statistics[0]?.games?.appearances}
+                  Goals={play?.statistics[0]?.goals?.total}
+                  Passes={play?.statistics[0]?.passes?.total}
+                  Cards={play?.statistics[0]?.cards?.yellowred}
+                />
+              ))}
+            </View>
+          ) : (
+            <>
+              <NotFound />
+            </>
+          )}
+        </>
+      )}
     </SafeAreaView>
   );
 };
